d95c488b39d9bcc5273ab987acde02c74db65d31
chat-wane <grumpy.chat.wane@gmail.com>
2017-05-05 11:24:40 +0200

    small change, todo, remove registering system

--- build/unicast-definition.bundle.js
+++ build/unicast-definition.bundle.js
@@ -18151,105 +18151,11 @@ process.chdir = function (dir) {
     throw new Error('process.chdir is not supported');
 };
 
-},{}],11:[function(require,module,exports){
-/**
- * Convert array of 16 byte values to UUID string format of the form:
- * XXXXXXXX-XXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
- */
-var byteToHex = [];
-for (var i = 0; i < 256; ++i) {
-  byteToHex[i] = (i + 0x100).toString(16).substr(1);
-}
-
-function bytesToUuid(buf, offset) {
-  var i = offset || 0;
-  var bth = byteToHex;
-  return  bth[buf[i++]] + bth[buf[i++]] +
-          bth[buf[i++]] + bth[buf[i++]] + '-' +
-          bth[buf[i++]] + bth[buf[i++]] + '-' +
-          bth[buf[i++]] + bth[buf[i++]] + '-' +
-          bth[buf[i++]] + bth[buf[i++]] + '-' +
-          bth[buf[i++]] + bth[buf[i++]] +
-          bth[buf[i++]] + bth[buf[i++]] +
-          bth[buf[i++]] + bth[buf[i++]];
-}
-
-module.exports = bytesToUuid;
-
-},{}],12:[function(require,module,exports){
-(function (global){
-// Unique ID creation requires a high quality random # generator.  In the
-// browser this is a little complicated due to unknown quality of Math.random()
-// and inconsistent support for the `crypto` API.  We do the best we can via
-// feature-detection
-var rng;
-
-var crypto = global.crypto || global.msCrypto; // for IE 11
-if (crypto && crypto.getRandomValues) {
-  // WHATWG crypto RNG - http://wiki.whatwg.org/wiki/Crypto
-  var rnds8 = new Uint8Array(16);
-  rng = function whatwgRNG() {
-    crypto.getRandomValues(rnds8);
-    return rnds8;
-  };
-}
-
-if (!rng) {
-  // Math.random()-based (RNG)
-  //
-  // If all else fails, use Math.random().  It's fast, but is of unspecified
-  // quality.
-  var  rnds = new Array(16);
-  rng = function() {
-    for (var i = 0, r; i < 16; i++) {
-      if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
-      rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
-    }
-
-    return rnds;
-  };
-}
-
-module.exports = rng;
-
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],13:[function(require,module,exports){
-var rng = require('./lib/rng');
-var bytesToUuid = require('./lib/bytesToUuid');
-
-function v4(options, buf, offset) {
-  var i = buf && offset || 0;
-
-  if (typeof(options) == 'string') {
-    buf = options == 'binary' ? new Array(16) : null;
-    options = null;
-  }
-  options = options || {};
-
-  var rnds = options.random || (options.rng || rng)();
-
-  // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
-  rnds[6] = (rnds[6] & 0x0f) | 0x40;
-  rnds[8] = (rnds[8] & 0x3f) | 0x80;
-
-  // Copy bytes to buffer, if provided
-  if (buf) {
-    for (var ii = 0; ii < 16; ++ii) {
-      buf[i + ii] = rnds[ii];
-    }
-  }
-
-  return buf || bytesToUuid(rnds);
-}
-
-module.exports = v4;
-
-},{"./lib/bytesToUuid":11,"./lib/rng":12}],"unicast-definition":[function(require,module,exports){
+},{}],"unicast-definition":[function(require,module,exports){
 'use strict';
 
 const debug = require('debug')('unicast-definition');
 const _ = require('lodash');
-const uuid = require('uuid/v4');
 
 const IUnicast = require('./interfaces/iunicast.js');
 
@@ -18263,9 +18169,14 @@ const ExMessage = require('./exceptions/exmessage.js');
 class Unicast {
     /**
      * @param {IPSP} psp The peer-sampling protocol.
+     * @param {object} [options] The options of this unicast.
+     * @param {string} [options.uid = 'default-unicast'] The name of this
+     * unicast.
+     * @param {number} [option.retry = 0] The number of attempt to send a
+     * message.
      */
     constructor(psp, options = {}) {        
-        this.options = ( {retry: 0, uid: uuid()}, options);
+        this.options = ( {retry: 0, uid: 'default-unicast'}, options);
         // #1 create the table of registered protocols
         this.psp = psp;
         this.protocols = new Map();
@@ -18330,4 +18241,4 @@ class Unicast {
 
 module.exports = Unicast;
 
-},{"./exceptions/exmessage.js":1,"./exceptions/exprotocol.js":2,"./interfaces/iunicast.js":3,"debug":5,"lodash":8,"uuid/v4":13}]},{},[]);
+},{"./exceptions/exmessage.js":1,"./exceptions/exprotocol.js":2,"./interfaces/iunicast.js":3,"debug":5,"lodash":8}]},{},[]);
